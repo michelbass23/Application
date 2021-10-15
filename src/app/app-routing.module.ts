bf083c973187a50fe3d3b3ff07d618165aee972f
Vanessa Lopes <63751937+NeuVanessa@users.noreply.github.com>
2021-10-15 16:55:55 -0300

    create page login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   }
 ];
 @NgModule({
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController} from 'ionic-angular';
+import { HomePage } from "../../pages/home/home.page";
+
 
 @Component({
   selector: 'app-login',
@@ -7,11 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
-
+  irHome(){
+    this.navCtrl.push(HomePage, {}, {animate: true} );    
+  }
   
 }
