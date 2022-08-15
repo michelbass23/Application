08b4fa0bd0437fcd40bbcab1fe6e4b60dc49be6a
Michel Ribeiro <michelbass23@gmail.com>
2022-08-15 20:25:43 -0300

    Ajustes
    ajustes no comportamento das paginas

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HomePage } from "../../pages/home/home.page";
-import { LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -9,8 +9,14 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+email:'michelbass23@gmail.com';
+senha:'@ext1234';
 
-  constructor(private loadCtrl:LoadingController,private navCtrl:NavController) { }
+  constructor(private loadCtrl:LoadingController,
+    private navCtrl:NavController,
+    private toastCtrl:ToastController,
+    private alertCtrl:AlertController
+    ) { }
 
   ngOnInit() {
 
@@ -20,8 +26,42 @@ export class LoginPage implements OnInit {
       this.navCtrl.navigateRoot('registrar');
     }
 
- 
+     async login(){
+      if(this.email=='michelbass23@gmail.com'&&this.senha=='@ext1234'){
+        const loading = await this.loadCtrl.create({
+        message:"Aguarde",
+        duration:2000,
+      });
+      this.presentAlert();
+      this.navCtrl.navigateForward('/home');  
+     
+    }else{
+      this.presentToast();
+    }
 
-  }
-  
+
+    }
+ 
+      async presentToast() {
+        const toast = await this.toastCtrl.create({
+          message: 'Login ou Senha inválidos!',
+          duration: 2000
+        });
+        toast.present();
+      }
+
+
+      async presentAlert() {
+        const alert = await this.alertCtrl.create({
+          header: 'Bem vindo',
+          buttons: ['OK'],
+          
+        });
+    
+        await alert.present();
+      }
+    }
+
+    
+    
 
